26af4c9372e2475047db82aa35917f8971b9d91a
h0sang <DonkeyJewi@gmail.com>
2018-08-29 00:33:21 +0300

    JS5end

---
+++ JS learn/Lesson 5/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+alert('Hello everyone');
+
+var message;
+    message = 'Hi body';
+
+var name = 'John';
+
+var age = 25, country = 'Ukraine';
+
+alert(message);
+alert(name);
+alert(age);
+alert(country);
+
+var $ = 1;
+var _ = 2;
+
+alert($ + _);
+
+var apple = 5,
+    Apple = 6;
+
+alert('var apple = ' + apple);
+alert('var Apple = ' + Apple);
+
+// doesn't work because there's no var
+// num = 45;
+// alert(num);
+// doesn't work because there's no var
+
+var COLOR_BLUE = '#00F';
+
+var color = COLOR_BLUE;
+alert(color);
+
+// Exercise 1
+
+var admin, name;
+    name = 'Василий';
+    admin = name;
+
+alert(admin);
+
+let $qwe = 4;
+alert($qwe);
