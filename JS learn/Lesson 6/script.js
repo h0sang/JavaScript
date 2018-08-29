05eefc487be8dd1ff89a223035da82afc3b98596
h0sang <DonkeyJewi@gmail.com>
2018-08-29 23:43:31 +0300

    Lesson 6

---
+++ JS learn/Lesson 6/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*
+  var moiTovari;
+  var cena;
+  var ssilka;
+*/
+
+var myGoods;
+var price;
+var link;
+
+var planet = 'Earth',
+    userName = 'Kate';
+
+"use strict";
+
+var $i = 0,                 // Hi, it's me
+    mySkill = 0,            // Like usually
+    playJumping = true,     // Win
+    playStream_map = false, // lose
+    what_ihave;             // My result
+
+  switch($i) {
+    case playJumping:
+      console.log('How about sotarks map?!');
+      what_ihave = '+10pp';
+      break;
+    case playStream_map:
+      console.log('I will suck');
+      what_ihave = 'NOTHING';
+      break;
+    default:
+      console.log('just go away');
+      what_ihave = 'I dead';
+      break;
+  }
