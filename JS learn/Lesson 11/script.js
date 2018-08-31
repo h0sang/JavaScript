c00b24f2e56560cca01fbdb6a0ec3d56d83bee60
h0sang <DonkeyJewi@gmail.com>
2018-09-01 00:10:07 +0300

    JS11end

---
+++ JS learn/Lesson 11/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+alert( 'hey' );
+
+var year = prompt('how old are you?', 100);
+alert("I'm " + year + "years old!");
+
+var isAdmin = confirm('Do you admin?');
+
+alert(isAdmin);
+
+
+var name = prompt('What is your name?', '');
+alert('Welcome ' + name);
